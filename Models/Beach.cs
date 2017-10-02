56ce83b87640f5b541e1766598f1eea77c5d3296
Alexandre Campinho <alexandrearc@gmail.com>
2017-10-02 21:15:16 +1300

    Update required fields.

--- Models/Beach.cs
+++ Models/Beach.cs
@@ -6,6 +6,20 @@ namespace SurfLog.Api.Models {
         
         [Key]
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+        
+        public string Description { get; set; }
+        
+        public string Type { get; set; }
+        
+        public string Swell { get; set; }
+        
+        public string Wind { get; set; }
+        
+        public string Skill { get; set; }
+
+        public string Ground { get; set; }
     }
 }
--- Models/Session.cs
+++ Models/Session.cs
@@ -7,11 +7,20 @@ namespace SurfLog.Api.Models
     {
         [Key]
         public int Id { get; set; }
+        
+        [Required]
         public Beach Beach { get; set; }
+        
+        [Required]
         public DateTime Date { get; set; }
+        
         public string Description { get; set; }
+
         public decimal Duration { get; set; }
+
         public int Rating { get; set; }
+
+        [Required]
         public string UserId { get; set;}
     }
 }
